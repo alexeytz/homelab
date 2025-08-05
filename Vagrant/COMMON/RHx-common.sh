ead08354d359204b3223f9cf4d8ae91aa51895df
alexey.tsygankov@gmail.com <alexey.tsygankov@gmail.com>
2025-08-05 14:13:48 -0400

    Vagrant configuration for K8 envs RH8 and RH9 based

---
+++ Vagrant/COMMON/RHx-common.sh
@@ -0,0 +1,14 @@
+cat $0
+echo Executing common customization script: $0
+
+echo " [+] $(date) Stop and Disable firewall..."
+systemctl stop firewalld
+systemctl disable firewalld
+
+echo " [+] $(date) Set /etc/resolv.conf..."
+rm -f /etc/resolv.conf
+cat >/etc/resolv.conf<<EOF
+options timeout:30
+nameserver $NAMESERVER
+EOF
+chattr +i /etc/resolv.conf
