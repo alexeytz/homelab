14c06486f18c85435650b17474cf2459d287e5b9
alexeytz <alexey.tsygankov@gmail.com>
2026-02-19 19:09:08 -0500

    +Syncthing

---
+++ ProxMox/OpenMediaVault/SyncThing/readme.md
@@ -0,0 +1,5 @@
+# Syncthing is an excellent tool for remote sync/backup
+
+[Windows installer](https://syncthing.net/downloads/)
+[GitHub](https://github.com/syncthing/syncthing)
+[Documentation](https://docs.syncthing.net/)
---
+++ Ubuntu/additional_packages.sh
@@ -0,0 +1,4 @@
+apt install -y lrzsz tree
+
+# lrzsz - package providing tools (lrz and lsz) to transfer files over serial ports or terminal
+#         emulators using error-correcting protocols like ZMODEM, XMODEM, and YMODEM. Used by WindTerm.
